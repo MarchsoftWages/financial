cf1368d7b7d6abb4e61a9bd86904d4d30a5d6c3a
chaimengchen <1615542960@qq.com>
2017-10-29 22:30:27 +0800

    10.9

---
+++ app/Http/Controllers/admin/codeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use Gregwar\Captcha\CaptchaBuilder;
+use Session;
+
+class codeController extends Controller{
+	/*
+     获取验证码
+	*/
+	public function get_captcha($temp){
+		$builder=new CaptchaBuilder();
+		$builder ->build(100,32);
+		$phrase = $builder->getPhrase();
+		Session::flash('milkcaptcha',$phrase);
+		ob_clean();
+		return response($builder->output())->header('Content-type','image/jpeg');
+	}
+	public function get_vaild(Request $request){
+       
+	}
+}
