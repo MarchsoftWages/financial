593157fe88c14edc77104edea07ebddcf239a320
chaimengchen <1615542960@qq.com>
2017-10-30 20:28:16 +0800

    10.30

--- app/Http/Controllers/admin/LoginController.php
+++ app/Http/Controllers/admin/LoginController.php
@@ -6,6 +6,8 @@ use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Models\Admin;
 use Session;
+
+
 class LoginController extends Controller
 {
     public function get_vaild(Request $request){
@@ -19,27 +21,28 @@ class LoginController extends Controller
         	$user_name=$data['name'];
         	$user_password=$data['password'];
         	$user_captcha=$data['Captcha'];
-            $this->validate($request,[
+
+            $this ->validate($request,[
                 'user.name'=>'regex:/^\d{6}$/',
-                'user.password'=>'required|mix:6|regex:/^\w{6,}$/',
-                'user.Captcha'=>'required regex:/^[0-9a-zA_Z]{5}$/',
+                'user.password'=>'required|min:6|regex:/^\w{6,}$/',
+                'user.Captcha'=>'required|min:5| regex:/^[0-9a-zA_Z]{5}$/',
                 ],[
                 'required' => ':attribute不能为空',
                  'min' => ':attribute最小输入两个字符',
-                 'max' =>':attribute最大输入20个字符',
-                 'User.name.regex' => ':attribute输入错误',
-                ]，[
-                  'user.name'=>'用户名'，
+                 'user.name.regex' => ':attribute输入错误',
+                ],[
+                  'user.name'=>'用户名',
                   'user.password'=>'密码',
                   'user.Captcha'=>'验证码',
-                ])
+                ]);
         }
+       
         if(($name==$user_name)&&($password==md5(md5($user_password)))){
             if(Session::get('milkcaptcha')==$user_captcha){
                 session(['login' => 'true']);
+                var_dump("aaa");
+                // return redirect('/back_index');
             }
-        }else{
-
         }
 
 	}
