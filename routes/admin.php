01a4c6cc035b5b9d3661762a15ec57e90427f816
seapower <1340639717@qq.com>
2017-11-03 22:47:08 +0800

    覆盖上传

--- routes/admin.php
+++ routes/admin.php
@@ -1,12 +1,13 @@
 <?php
 
 Route::post('login','admin\LoginController@get_vaild');
-Route::group(['middleware'=>['login']],function(){
+//Route::group(['middleware'=>['login']],function(){
     Route::get('/back_index', function () {
         return view('index');
     });
     Route::get('/getlogs', 'admin\PayController@selectLog');
     Route::post("/admin/upload", 'admin\PayController@saveExcel');
-});
+    Route::post("/admin/update", 'admin\PayController@updateExcel');
+//});
 
 
