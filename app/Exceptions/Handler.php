184f91d033240487094d1b61b549f2437f99cd11
wangihang <qihang_wang@qq.com>
2017-11-15 16:04:44 +0800

    404页面

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,6 +44,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception) {
+            return response()->view('error.'.$exception->getStatusCode(), [],$exception->getStatusCode());
+        }
         return parent::render($request, $exception);
     }
 
