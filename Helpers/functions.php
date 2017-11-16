302863068910b1f32c922766ad36dbcfdd7e6b39
wangihang <qihang_wang@qq.com>
2017-11-16 15:20:32 +0800

    wx

--- Helpers/functions.php
+++ Helpers/functions.php
@@ -25,3 +25,18 @@ function get_session_user_id()
     $user = session("user");
     return $user ? $user->id : 0;
 }
+function curlGet($url,$method,$post_data = 0){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    if ($method == 'post') {
+        curl_setopt($ch, CURLOPT_POST, 1);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+    }elseif($method == 'get'){
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+    }
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
